c2a6630d375f41addaef7826e6227a6232a2f53a
Hirako <hirako2000@gmail.com>
2020-08-15 17:54:46 +0700

    Update docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker pull -t "${SRC_USER}/${SRC_IMAGE}"
+docker pull "${SRC_USER}/${SRC_IMAGE}"
 chmod 755 dist_linux/docker-slim
 dist_linux/docker-slim build --target "${SRC_USER}/${SRC_IMAGE}"
 docker tag "${SRC_USER}/${SRC_IMAGE}" "${DOCKER_USERNAME}/${DST_IMAGE}"
